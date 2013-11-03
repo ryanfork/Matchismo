f2e8eab2f22f39e9cc85722c1d723bf05f21f01d
ryanfork <rjfork@gmail.com>
2013-11-02 20:51:32 -0400

    Add classes.
    Not building -- need to find errors.

---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Ryan Fork on 11/2/13.
+//  Copyright (c) 2013 Ryan's App House. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
